12adfe263a69eeeb9ff1a1937cbd83fb4407b27e
Mike Kelly <mike@membervault.co>
2020-03-23 09:46:33 -0700

    start of script

---
+++ install-lamp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "*************************************"
+echo "* Installing Apache, PHP, and MySql *"
+echo "*************************************"
+
+
+sudo apt install apache2 php libapache2-mod-php mariadb-server
