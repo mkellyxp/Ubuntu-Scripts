e32d222c41079fc4ab3c1ba222713a6d4e3aa6eb
Mike Kelly <mike@membervault.co>
2020-07-05 13:36:40 -0700

    Add curl

--- install-lamp.sh
+++ install-lamp.sh
@@ -13,7 +13,7 @@ else
     echo "* Installing Apache, PHP, and MySql *"
     echo "*************************************"
 
-    sudo apt install apache2 php libapache2-mod-php mariadb-server php-mysql php-mbstring php-xml php-gd
+    sudo apt install apache2 php libapache2-mod-php mariadb-server php-mysql php-mbstring php-xml php-gd php-curl
     sudo a2enmod rewrite
 
     echo ""
