0986ce38a85785b12f27190aa775f68f29c9d194
Mike Kelly <mike@membervault.co>
2022-05-01 01:48:45 -0700

    version 16

--- install-node.sh
+++ install-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
