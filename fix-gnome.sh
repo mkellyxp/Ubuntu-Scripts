0cc908845654f82cd6a4986178ba6cee7dc2ba8e
Michael Kelly <mike@thegrumpydev.com>
2022-05-27 19:02:22 -0700

    Update fix-gnome.sh

--- fix-gnome.sh
+++ fix-gnome.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+gsettings set org.gnome.desktop.privacy remember-recent-files false
+
 nvim ~/.config/user-dirs.dirs
+
+
+# Brightness
+# https://extensions.gnome.org/extension/1625/soft-brightness/
